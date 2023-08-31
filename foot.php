5f06f36183ec6ec26202041cf2ff96e183530255
jpmeijers <git@jpmeijers.com>
2023-08-31 17:39:04 +0200

    https://github.com/cdnjs/cdnjs/discussions/14124 https://github.com/cdnjs/cdnjs/issues/14080

--- foot.php
+++ foot.php
@@ -22,12 +22,12 @@
 <script src="/libs/he/he.js"></script>
 
 <!-- Moment for datetime manipulation -->
-<script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.29.4/moment.min.js"
-        integrity="sha512-CryKbMe7sjSCDPl18jtJI5DR5jtkUWxPXWaLCst6QjH8wxDexfRJic2WRmRXmstr2Y8SxDDWuBO6CQC6IE4KTA=="
-        crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/moment-timezone/0.5.43/moment-timezone-with-data.js"
-        integrity="sha512-lWuI1XloUBHbHQzrGW+f/uAuFA9rvHF5GsAU8Yo1mRevjkG2uChlWTueootXyWRCVtVjpxe85x18CZEt7o62Iw=="
-        crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+<script src="
+https://cdn.jsdelivr.net/npm/moment@2.29.4/moment.min.js
+"></script>
+<script src="
+https://cdn.jsdelivr.net/npm/moment-timezone@0.5.43/moment-timezone.min.js
+"></script>
 
 <script>
     $(function () {
